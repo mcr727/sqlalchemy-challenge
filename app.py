51a2a9f7f7762b3eca84cb29d91b2d78494fff2a
mcr727 <michael.reichard@gmail.com>
2021-02-03 15:57:25 -0500

    added homework files

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+
+from flask import Flask, jsonify
+
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+base = automap_base()
+base.prepare(engine, reflect=True)
+measurement = base.classes.measurement
+station = base.classes.station
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start_date><br/>"
+        f"/api/v1.0/<start_date>/<end_date><br/>"
+    )
